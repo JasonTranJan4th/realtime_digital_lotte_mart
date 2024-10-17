d26c42edf6749a5a874ab50d5401ba86a61f5951
Jason Tran <duycuong.drg@gmail.com>
2024-10-17 15:13:43 +0700

    update API using https instead http

--- api/axiosClient.js
+++ api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: 'http://192.168.1.17:5158/api',
+    baseURL: 'https://192.168.1.17:7190/api',
     headers: {
         'Content-Type': 'application/json',
     },
